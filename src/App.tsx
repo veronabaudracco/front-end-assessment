fcc0d5bac48f920f3341455a2fb3a22df97d5fe1
veronabaudracco <veronabaudracco@gmail.com>
2024-09-22 14:34:11 +0200

    add logic to initialize cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import './App.css'
-import Header from './components/header'
+import { useEffect, useState } from "react";
+import "./App.css";
+import CardGrid from "./components/CardGrid/CardGrid";
+import Header from "./components/Header";
+import { MemoryCard } from "./components/Card";
+import getCardsData from "./utils";
 
-const App = () => (
+const App = () => {
+  const [cards, setCards] = useState<MemoryCard[]>([]);
+
+  const initialCards = getCardsData().map((card) => ({
+    ...card,
+    flipped: false,
+    matched: false,
+  }));
+
+  useEffect(() => {
+    setCards(initialCards);
+  }, []);
+
+  const handleResetGame = () => {
+    setCards(initialCards);
+  };
+
+  return (
     <>
-      <Header />
+      <Header onReset={handleResetGame} />
+      <CardGrid cards={cards} setCards={setCards} />
     </>
-)
+  );
+};
 
-export default App
+export default App;
